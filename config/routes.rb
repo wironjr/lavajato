9fc3509e3f4789821886d0439a654652a647a495
wironjr <jun1n_@live.com>
2023-03-14 15:01:15 -0300

    cria static pages index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  root to: "static_pages#index"
   resources :servicos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
