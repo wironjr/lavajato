a392438dff03cbb46b60f21ae3f46937a8b044bc
wironjr <jun1n_@live.com>
2023-03-29 21:44:53 -0300

    ajuste na logica financeiro

--- app/controllers/financas_controller.rb
+++ app/controllers/financas_controller.rb
@@ -12,6 +12,9 @@ class FinancasController < ApplicationController
     @servicos_lucro = @servicos_total - @despeses_mes
     @servicos_quantidade_mes = @servicos.where(data: Time.now.beginning_of_month..Time.now.end_of_month).count
     @servicos_pago_pendedente = @servicos.where(data: Time.now.beginning_of_month..Time.now.end_of_month).where(pago: :false).count
+    @servicos_mensal = @servicos.where(data: Time.now.beginning_of_month..Time.now.end_of_month)
+    @despesas_produtos_mes = @despesas.where(tipo: "PRODUTOS").where(data: Time.now.beginning_of_month..Time.now.end_of_month).sum(:valor)
+    @despesas_geral_mes = @despesas.where.not(tipo: "PRODUTOS").where(data: Time.now.beginning_of_month..Time.now.end_of_month).sum(:valor)
     
     if params[:mes_select]
       @mes = params[:mes_select] || Time.now.month
@@ -20,15 +23,12 @@ class FinancasController < ApplicationController
       @servicos_total = @servicos_mensal.map(&:valor).sum
       @servicos_lucro = @servicos_total - @despeses_mes
       @servicos_quantidade_mes = @servicos_mensal.count
-      @ticketmedio_mes = @servicos_lucro / @servicos_quantidade_mes if @servicos_quantidade_mes != 0
+      @despesas_produtos_mes =  @despesas.where("DATE_PART('month', data) = ?", @mes).where(tipo: "PRODUTOS").sum(:valor)
+      @despesas_geral_mes =  @despesas.where("DATE_PART('month', data) = ?", @mes).where.not(tipo: "PRODUTOS").sum(:valor)
+      @ticketmedio_mes = @servicos_lucro / @servicos_quantidade_mes if params[:mes_select].present? && @servicos_quantidade_mes != 0 
     else
-      @servicos_mensal = @servicos.where(data: Time.now.beginning_of_month..Time.now.end_of_month)
+      @ticketmedio_mes = @servicos_lucro / @servicos_quantidade_mes if @servicos_quantidade_mes != 0
     end
-
-    @despesas_produtos_mes = @despesas.where(tipo: "PRODUTOS").where(data: Time.now.beginning_of_month..Time.now.end_of_month).sum(:valor)
-    @despesas_geral_mes = @despesas.where.not(tipo: "PRODUTOS").where(data: Time.now.beginning_of_month..Time.now.end_of_month).sum(:valor)
-
-    @ticketmedio_mes = @servicos_lucro / @servicos_quantidade_mes if @servicos_quantidade_mes != 0
   end
 
   def individual
