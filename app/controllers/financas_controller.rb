6a1a34d603516742d4363a3a2a75fd7d7c76ce63
wironjr <jun1n_@live.com>
2023-03-31 14:31:58 -0300

    adiciona index financeiro e alguns ajustes

--- app/controllers/financas_controller.rb
+++ app/controllers/financas_controller.rb
@@ -1,7 +1,21 @@
 class FinancasController < ApplicationController
   before_action :require_logged_in_user
+  
   def index
-    @nome=12
+    @servicos = Servico.all
+    @despesas = Despesa.all
+    @servicos_total = @servicos.where(pago: :true).map(&:valor).sum
+    @despeses_mes = @despesas.sum(:valor)
+    @servicos_lucro = @servicos_total - @despeses_mes
+    @servicos_quantidade = @servicos.count
+    @despesas_valor = @despesas.sum(:valor)
+    
+    @despesas_produtos_mes = @despesas.where(tipo: "PRODUTOS").sum(:valor)
+    @despesas_geral_mes = @despesas.where.not(tipo: "PRODUTOS").sum(:valor)
+
+    @ticketmedio_mes = @servicos_lucro / @servicos_quantidade if @servicos_quantidade != 0
+    @despesas_media_valor = @despesas_valor / @despesas.count if @despesas.count > 0
+    
   end
 
   def mensal
@@ -13,13 +27,19 @@ class FinancasController < ApplicationController
     @servicos_quantidade_mes = @servicos.where(data: Time.now.beginning_of_month..Time.now.end_of_month).count
     @servicos_pago_pendedente = @servicos.where(data: Time.now.beginning_of_month..Time.now.end_of_month).where(pago: :false).count
     @servicos_mensal = @servicos.where(data: Time.now.beginning_of_month..Time.now.end_of_month)
+    @despesas_mensal = @despesas.where(data: Time.now.beginning_of_month..Time.now.end_of_month)
+    @despesas_valor = @despesas_mensal.sum(:valor)
+    
     @despesas_produtos_mes = @despesas.where(tipo: "PRODUTOS").where(data: Time.now.beginning_of_month..Time.now.end_of_month).sum(:valor)
     @despesas_geral_mes = @despesas.where.not(tipo: "PRODUTOS").where(data: Time.now.beginning_of_month..Time.now.end_of_month).sum(:valor)
     
     if params[:mes_select]
       @mes = params[:mes_select] || Time.now.month
       @servicos_mensal = @servicos.where("DATE_PART('month', data) = ?", @mes) if params[:mes_select].present?
+      @despesas_mensal = @despesas.where("DATE_PART('month', data) = ?", @mes) if params[:mes_select].present?
       @despeses_mes = @despesas.where("DATE_PART('month', data) = ?", @mes).sum(:valor)
+      @despesas_valor = @despesas_mensal.sum(:valor)
+      @despesas_media_valor = @despesas_valor / @despesas_mensal.count if @despesas_mensal.count > 0 
       @servicos_total = @servicos_mensal.map(&:valor).sum
       @servicos_lucro = @servicos_total - @despeses_mes
       @servicos_quantidade_mes = @servicos_mensal.count
@@ -28,6 +48,7 @@ class FinancasController < ApplicationController
       @ticketmedio_mes = @servicos_lucro / @servicos_quantidade_mes if @servicos_quantidade_mes != 0 
     else
       @ticketmedio_mes = @servicos_lucro / @servicos_quantidade_mes if @servicos_quantidade_mes != 0
+      @despesas_media_valor = @despesas_valor / @despesas_mensal.count if @despesas_mensal.count > 0
     end
   end
 
