36b2ebd82be0f903c63afa1885259d9b3eb798e5
wironjr <jun1n_@live.com>
2023-05-16 15:45:00 -0300

    adiciona ferramenta de busca em despesas

--- app/controllers/despesas_controller.rb
+++ app/controllers/despesas_controller.rb
@@ -8,7 +8,7 @@ class DespesasController < ApplicationController
     @despesas = Despesa.where.not(tipo: "FUNCIONÁRIO").order(:data)
     @despesas_maior_valor2 = @despesas.maximum(:valor)
     @despesa_fixa_func = Despesa.where(tipo: "FUNCIONÁRIO")
-    @user = User.where(desligamento: "01-01-3000").where(tipo: "FUNCIONÁRIO")
+    @user = User.where(desligamento: "3000-01-01").where(tipo: "FUNCIONÁRIO")
     
     if params[:mes_select]
       user_ids = User.where(tipo: "FUNCIONÁRIO").where("to_char(date_trunc('month', created_at), 'YYYY-MM') <= '#{params[:mes_select]}' AND to_char(date_trunc('month', desligamento), 'YYYY-MM') >= '#{params[:mes_select]}'").pluck(:id)
@@ -51,6 +51,28 @@ class DespesasController < ApplicationController
     @despesa_func_qnt = @despesas.where(tipo: "FUNCIONÁRIO").count
     @despesa_func_valor = @despesas.where(tipo: "FUNCIONÁRIO").sum(:valor)
 
+    if params[:mes_select] || params[:nome_despesa_busca]
+      @mes = params[:mes_select]
+      ano = @mes.split("-")[0].to_i
+      mes = @mes.split("-")[1].to_i
+      @despesas = @despesas.where("DATE_PART('year', data) = ? AND DATE_PART('month', data) = ?", ano, mes) if params[:mes_select].present?
+      @despesas = @despesas.where('tipo ILIKE ?', "%#{params[:nome_despesa_busca]}%") if params[:nome_despesa_busca].present?
+
+      @despesas_total = @despesas.sum(:valor)
+      @despesas_total_qnt = @despesas.count
+      @despesas_maior_valor = @despesas.max_by(&:valor)
+      @despesas_maior_valor2 = @despesas.maximum(:valor)
+
+      @despesa_vale_qnt = @despesas.where(tipo: "VALE").count
+      @despesa_vale_valor = @despesas.where(tipo: "VALE").sum(:valor)
+      
+      @despesa_produto_qnt = @despesas.where(tipo: "PRODUTOS").count
+      @despesa_produto_valor = @despesas.where(tipo: "PRODUTOS").sum(:valor)
+      
+      @despesa_func_qnt = @despesas.where(tipo: "FUNCIONÁRIO").count
+      @despesa_func_valor = @despesas.where(tipo: "FUNCIONÁRIO").sum(:valor)
+    end
+
     @pagy, @despesas = pagy(@despesas)
   end
 
@@ -60,11 +82,15 @@ class DespesasController < ApplicationController
     @despesas_total = @despesas.sum(:valor)
     @despesas_total_qnt = @despesas.count
 
-    if params[:mes_select]
+    if params[:mes_select] || params[:vale_nome_busca]
       @mes = params[:mes_select]
       ano = @mes.split("-")[0].to_i
       mes = @mes.split("-")[1].to_i
-      @despesas = @despesas.where("DATE_PART('year', data) = ? AND DATE_PART('month', data) = ?", ano, mes)
+      @despesas = @despesas.where("DATE_PART('year', data) = ? AND DATE_PART('month', data) = ?", ano, mes) if params[:mes_select].present?
+      @despesas = @despesas.where('vale ILIKE ?', "%#{params[:vale_nome_busca]}%") if params[:vale_nome_busca].present?
+
+      @despesas_total = @despesas.sum(:valor)
+      @despesas_total_qnt = @despesas.count
     end
 
     @pagy, @despesas = pagy(@despesas)
@@ -75,11 +101,15 @@ class DespesasController < ApplicationController
     @despesas_total = @despesas.sum(:valor)
     @despesas_total_qnt = @despesas.count
 
-    if params[:mes_select]
+    if params[:mes_select] || params[:nome_produto_busca]
       @mes = params[:mes_select]
       ano = @mes.split("-")[0].to_i
       mes = @mes.split("-")[1].to_i
-      @despesas = @despesas.where("DATE_PART('year', data) = ? AND DATE_PART('month', data) = ?", ano, mes)
+      @despesas = @despesas.where("DATE_PART('year', data) = ? AND DATE_PART('month', data) = ?", ano, mes) if params[:mes_select].present?
+      @despesas = @despesas.where('observacao ILIKE ?', "%#{params[:nome_produto_busca]}%") if params[:nome_produto_busca].present?
+
+      @despesas_total = @despesas.sum(:valor)
+      @despesas_total_qnt = @despesas.count
     end
 
     @pagy, @despesas = pagy(@despesas)
@@ -89,6 +119,18 @@ class DespesasController < ApplicationController
     @despesas = Despesa.where.not(tipo: ["PRODUTOS", "VALE"]).order(:data)
     @despesas_total = @despesas.sum(:valor)
     @despesas_total_qnt = @despesas.count
+
+    if params[:mes_select] || params[:nome_despesa_busca]
+      @mes = params[:mes_select]
+      ano = @mes.split("-")[0].to_i
+      mes = @mes.split("-")[1].to_i
+      @despesas = @despesas.where("DATE_PART('year', data) = ? AND DATE_PART('month', data) = ?", ano, mes) if params[:mes_select].present?
+      @despesas = @despesas.where('tipo ILIKE ?', "%#{params[:nome_despesa_busca]}%") if params[:nome_despesa_busca].present?
+
+      @despesas_total = @despesas.sum(:valor)
+      @despesas_total_qnt = @despesas.count
+    end
+
     @pagy, @despesas = pagy(@despesas)
   end
 
@@ -96,19 +138,25 @@ class DespesasController < ApplicationController
   def new
     @despesa = Despesa.new
     @users = User.all
-    @users_func = User.where(tipo: ["FUNCIONÁRIO", "FUNCIONARIO COM ACESSO"]).where(desligamento: "01-01-3000")
+    users_func = User.where(tipo: ["FUNCIONÁRIO", "FUNCIONARIO COM ACESSO"]).where(desligamento: "3000-01-01") 
+    users_normal = User.where.not(tipo: ["FUNCIONÁRIO", "FUNCIONARIO COM ACESSO"])
+    @users_select = users_normal + users_func
+    @users_func = User.where(tipo: ["FUNCIONÁRIO", "FUNCIONARIO COM ACESSO"]).where(desligamento: "3000-01-01")
   end
 
   # GET /despesas/1/edit
   def edit
     @users = User.all
-    @users_func = User.where(tipo: ["FUNCIONÁRIO", "FUNCIONARIO COM ACESSO"]).where(desligamento: "01-01-3000")
+    users_func = User.where(tipo: ["FUNCIONÁRIO", "FUNCIONARIO COM ACESSO"]).where(desligamento: "3000-01-01") 
+    users_normal = User.where.not(tipo: ["FUNCIONÁRIO", "FUNCIONARIO COM ACESSO"])
+    @users_select = users_normal + users_func
+    @users_func = User.where(tipo: ["FUNCIONÁRIO", "FUNCIONARIO COM ACESSO"]).where(desligamento: "3000-01-01")
   end
 
   # POST /despesas or /despesas.json
   def create
     @users = User.all
-    @users_func = User.where(tipo: ["FUNCIONÁRIO", "FUNCIONARIO COM ACESSO"]).where(desligamento: "01-01-3000")
+    @users_func = User.where(tipo: ["FUNCIONÁRIO", "FUNCIONARIO COM ACESSO"]).where(desligamento: "3000-01-01")
     params[:despesa][:valor] = params[:despesa][:valor].gsub('R$','').gsub(' ','').gsub('.','')
     
     @despesa = Despesa.new(despesa_params)
@@ -121,24 +169,17 @@ class DespesasController < ApplicationController
         case @despesa.tipo
           when "PRODUTOS"
             flash[:success] = "Produto criado com sucesso!"
-          when "GERAL"
-            flash[:success] = "Despesa geral criada com sucesso!"
-          when "VALE"
-            flash[:success] = "Vale criado com sucesso!"
-          else
-            flash[:success] = "Despesa criada com sucesso!"
-        end
-        
-        case @despesa.tipo
-          when "PRODUTOS"
             redirect_to produtos_despesas_path
           when "GERAL"
+            flash[:success] = "Despesa geral criada com sucesso!"
             redirect_to geral_despesas_path
           when "VALE"
+            flash[:success] = "Vale criado com sucesso!"
             redirect_to vale_despesas_path
           else
+            flash[:success] = "Despesa criada com sucesso!"
             redirect_to despesas_path
-        end  
+        end
       else
         flash[:danger] = "Despesa não foi criada!"
         render 'new'
